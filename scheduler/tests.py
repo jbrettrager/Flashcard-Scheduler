d58b6b3ca568575b7ed9692b67e80faab0655b89
ax-matt <jbrettrager94@gmail.com>
2025-10-04 12:39:14 +0900

    add test for class serializer

--- scheduler/tests.py
+++ scheduler/tests.py
@@ -1,8 +1,15 @@
 from django.test import TestCase
 from scheduler.models import Flashcard
+from scheduler.serializers import FlashcardSerializer
+
 
 # Create your tests here.
 class FlashcardModelTest(TestCase):
     def test_flashcard_creation(self):
         card = Flashcard.objects.create(vocab="Testing")
         self.assertEqual(card.vocab, "Testing")
+
+    def test_serializer_validation(self):
+        data = {'vocab': 'Testing'}
+        serializer = FlashcardSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
