f219af57426866712d66042ab80a0123be1323e0
ax-matt <jbrettrager94@gmail.com>
2025-10-04 13:41:41 +0900

    add test for ReviewResult class
    Add test for theoretical ReviewResult class.

--- scheduler/tests.py
+++ scheduler/tests.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
-from scheduler.models import Flashcard
+from django.utils import timezone
+from datetime import timedelta
+
+from scheduler.models import Flashcard, ReviewResult, ReviewRating
 from scheduler.serializers import FlashcardSerializer
 
 
 # Create your tests here.
+# Flashcard Model Tests
 class FlashcardModelTest(TestCase):
     def test_flashcard_creation(self):
         card = Flashcard.objects.create(vocab="Testing")
@@ -13,3 +17,21 @@ class FlashcardModelTest(TestCase):
         data = {'vocab': 'Testing'}
         serializer = FlashcardSerializer(data=data)
         self.assertTrue(serializer.is_valid())
+
+# ReviewResult Model Tests
+class ReviewResultModelTest(TestCase):
+    def setUp(self):
+        self.userID = 100
+        self.flashcard = Flashcard.objects.create(vocab="Testing")
+
+    def test_create_review_result(self):
+        review = ReviewResult.objects.create(
+            userID=self.userID,
+            flashcard=self.flashcard,
+            rating=ReviewRating.INSTANT,
+            due_date=timezone.now() + timedelta(days=30),
+            idempotency_key='asdfjkl')
+        self.assertEqual(review.userID, self.userID)
+        self.assertEqual(review.flashcard, self.flashcard)
+        self.assertEqual(review.rating, ReviewRating.INSTANT)
+        self.assertEqual(review.idempotency_key, 'asdfjkl')
