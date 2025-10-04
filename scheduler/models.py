e4fcd01cf9b04b20d08f91401bcba01c8542251b
ax-matt <jbrettrager94@gmail.com>
2025-10-04 12:27:04 +0900

    create Flashcard class to pass test, migrate

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Flashcard(models.Model):
+    vocab = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.vocab
