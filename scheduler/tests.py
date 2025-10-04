4ed0f50967088c595c50cd51ba745110c8845839
ax-matt <jbrettrager94@gmail.com>
2025-10-04 12:25:17 +0900

    add test -> for flashcard model creation

--- scheduler/tests.py
+++ scheduler/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from scheduler.models import Flashcard
 
 # Create your tests here.
+class FlashcardModelTest(TestCase):
+    def test_flashcard_creation(self):
+        card = Flashcard.objects.create(vocab="Testing")
+        self.assertEqual(card.vocab, "Testing")
