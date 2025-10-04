9e7521f634d5ddbd49c287b7519937c8852be2d6
ax-matt <jbrettrager94@gmail.com>
2025-10-04 12:39:25 +0900

    add serializer for Flashcard class

---
+++ scheduler/serializers.py
@@ -0,0 +1,12 @@
+﻿from rest_framework import serializers
+from .models import Flashcard
+
+def validate_vocab(value):
+    if not value:
+        raise serializers.ValidationError('vocab cannot be empty')
+    return value
+class FlashcardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flashcard
+        fields = ['id','vocab']
+
