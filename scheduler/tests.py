54069a2dc388994b49ef3c33cf13d3b8ae58fff8
ax-matt <jbrettrager94@gmail.com>
2025-10-04 14:42:49 +0900

    Add tests for ReviewResultModel Serializer
    Also change the format of tests.py to separate the class creation and serializer tests.

--- scheduler/tests.py
+++ scheduler/tests.py
@@ -3,16 +3,18 @@ from django.utils import timezone
 from datetime import timedelta
 
 from scheduler.models import Flashcard, ReviewResult, ReviewRating
-from scheduler.serializers import FlashcardSerializer
+from scheduler.serializers import FlashcardSerializer, ReviewResultSerializer
 
 
 # Create your tests here.
+
 # Flashcard Model Tests
 class FlashcardModelTest(TestCase):
     def test_flashcard_creation(self):
         card = Flashcard.objects.create(vocab="Testing")
         self.assertEqual(card.vocab, "Testing")
 
+class FlashcardSerializerTest(TestCase):
     def test_serializer_validation(self):
         data = {'vocab': 'Testing'}
         serializer = FlashcardSerializer(data=data)
@@ -23,15 +25,56 @@ class ReviewResultModelTest(TestCase):
     def setUp(self):
         self.userID = 100
         self.flashcard = Flashcard.objects.create(vocab="Testing")
-
-    def test_create_review_result(self):
-        review = ReviewResult.objects.create(
+        self.review = ReviewResult.objects.create(
             userID=self.userID,
             flashcard=self.flashcard,
             rating=ReviewRating.INSTANT,
             due_date=timezone.now() + timedelta(days=30),
             idempotency_key='asdfjkl')
-        self.assertEqual(review.userID, self.userID)
-        self.assertEqual(review.flashcard, self.flashcard)
-        self.assertEqual(review.rating, ReviewRating.INSTANT)
-        self.assertEqual(review.idempotency_key, 'asdfjkl')
+
+    def test_create_review_result(self):
+
+        self.assertEqual(self.review.userID, self.userID)
+        self.assertEqual(self.review.flashcard, self.flashcard)
+        self.assertEqual(self.review.rating, ReviewRating.INSTANT)
+        self.assertEqual(self.review.idempotency_key, 'asdfjkl')
+
+class ReviewResultSerializerTest(TestCase):
+    def setUp(self):
+        self.userID = 101
+        self.flashcard = Flashcard.objects.create(vocab="Serializer Testing")
+
+    def test_serializer_validation(self):
+        serializer = ReviewResultSerializer(data={
+            'flashcard': self.flashcard.id,
+            'userID': self.userID,
+            'rating': ReviewRating.FORGOT,
+            'due_date': timezone.now() + timedelta(minutes=1),
+            'idempotency_key': 'serializerTest'
+        })
+        if not serializer.is_valid(): print(serializer.errors)
+        self.assertTrue(serializer.is_valid())
+        review_instance = serializer.save()
+
+        self.assertEqual(review_instance.userID, self.userID)
+        self.assertEqual(review_instance.flashcard, self.flashcard)
+        self.assertEqual(review_instance.rating, ReviewRating.FORGOT)
+        self.assertEqual(review_instance.idempotency_key, 'serializerTest')
+
+    def test_serializer_output(self):
+        review_instance = ReviewResult.objects.create(
+            flashcard=self.flashcard,
+            userID=self.userID,
+            rating=ReviewRating.FORGOT,
+            due_date=timezone.now() + timedelta(minutes=1),
+            idempotency_key='serializerTest'
+        )
+
+        serializer = ReviewResultSerializer(review_instance)
+        data = serializer.data
+
+        self.assertEqual(data['flashcard'], self.flashcard.id)
+        self.assertEqual(data['userID'], self.userID)
+        self.assertEqual(data['rating'], ReviewRating.FORGOT)
+        self.assertEqual(data['idempotency_key'], 'serializerTest')
+
