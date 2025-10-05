30e62b4316834b895e4429878ce02b1ce437a800
ax-matt <jbrettrager94@gmail.com>
2025-10-05 14:41:14 +0900

    update settings.py, add to project urlpatterns

--- Flashcard_Scheduler/settings.py
+++ Flashcard_Scheduler/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Flashcard_Scheduler.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'scheduler_db',
+        'USER': 'scheduler_user',
+        'PASSWORD': 'scheduler_user_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
