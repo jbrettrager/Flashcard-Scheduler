113dbfa062969271597d225149c7a538e13454b7
ax-matt <jbrettrager94@gmail.com>
2025-10-05 13:52:22 +0900

    commit missing files from /review implementation

---
+++ scheduler/services.py
@@ -0,0 +1,34 @@
+﻿from datetime import timedelta
+from django.utils import timezone
+from .models import ReviewResult
+
+def process_review(user_id, flashcard, rating, idempotency_key):
+    # Check idempotency_key
+    already_exists = ReviewResult.objects.filter(idempotency_key=idempotency_key).first()
+    if already_exists: return already_exists.due_date
+
+    last_review = ReviewResult.objects.filter(userID=user_id, flashcard=flashcard).order_by('-due_date').first()
+
+    now = timezone.now()
+
+    if rating == 0:
+        next_due = now + timedelta(minutes=1)
+    elif not last_review:
+        if rating == 1:
+            next_due = now + timedelta(days=5)
+        elif rating == 2:
+            next_due = now + timedelta(days=15)
+    else:
+        base = timedelta(days=15 if rating == 2 else 5)
+        next_due = max(last_review.due_date, now + base)
+
+    review = ReviewResult.objects.create(
+        userID=user_id,
+        flashcard=flashcard,
+        rating=rating,
+        idempotency_key=idempotency_key,
+        due_date=next_due,
+    )
+    return review.due_date
+
+
--- scheduler/views.py
+++ scheduler/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Flashcard
+from .serializers import ReviewResultSerializer
+from .services import process_review
+
 
 # Create your views here.
+class ReviewAPIView(APIView):
+    def post(self, request):
+        serializer = ReviewResultSerializer(data=request.data)
+        if serializer.is_valid():
+            review_instance = serializer.save()
+            due_date = process_review(
+                user_id=review_instance.userID,
+                flashcard=review_instance.flashcard,
+                rating=review_instance.rating,
+                idempotency_key=review_instance.idempotency_key,
+            )
+            return Response({'new_due_date': due_date})
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
