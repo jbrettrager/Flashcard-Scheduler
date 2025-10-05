a6f0531994bc2ae019bfb45a32920f281ce285a6
ax-matt <jbrettrager94@gmail.com>
2025-10-05 16:27:01 +0900

    add some default example cards to database
    these cards will automatically add after running py manage.py migrate

---
+++ scheduler/migrations/0004_initialize_cards.py
@@ -0,0 +1,20 @@
+﻿from django.db import migrations
+def initialize_cards(apps, schema_editor):
+    Flashcard = apps.get_model('scheduler', 'Flashcard')
+    cards = [
+        Flashcard(vocab='Indigenous'),
+        Flashcard(vocab='Tacticity'),
+        Flashcard(vocab='Lycoris'),
+        Flashcard(vocab='Deceitful'),
+        Flashcard(vocab='Spectroscopy')
+    ]
+    Flashcard.objects.bulk_create(cards)
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('scheduler', '0003_alter_reviewresult_due_date_and_more')
+    ]
+
+    operations = [
+        migrations.RunPython(initialize_cards)
+    ]
