4b3e03c1804d1875741ece7cf7349d1264ae4268
ax-matt <matt@axtormjapan.com>
2025-10-07 15:59:43 +0900

    use .json() when comparing responses from endpts
    passing in the entire response also compares metadata, so .json() is required if the resposnes are to be compared.

--- scheduler/tests.py
+++ scheduler/tests.py
@@ -216,9 +216,9 @@ class IdempotencyTest(APITestCase):
     def test_review_idempotency(self):
         first_response = self.client.post(self.review_url, self.review_payload_1, format='json')
         second_response = self.client.post(self.review_url, self.review_payload_1, format='json')
-        self.assertEqual(first_response, second_response)
+        self.assertEqual(first_response.json(), second_response.json())
 
     def test_due_cards_idempotency(self):
         first_response = self.client.get(self.due_cards_url, {'until': self.until_time.isoformat()}, format='json')
         second_response = self.client.get(self.due_cards_url, {'until': self.until_time.isoformat()}, format='json')
-        self.assertEqual(first_response, second_response)
+        self.assertEqual(first_response.json(), second_response.json())
